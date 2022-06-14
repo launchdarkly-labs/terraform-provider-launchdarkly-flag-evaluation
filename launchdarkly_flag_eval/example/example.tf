1bdf35b20f2bcda8c99c186cb883e497c68d871f
Isabelle Miller <imiller@launchdarkly.com>
2022-06-14 13:37:42 +0200

    add more to examples

--- launchdarkly_flag_eval/example/example.tf
+++ launchdarkly_flag_eval/example/example.tf
@@ -2,24 +2,28 @@ terraform {
   required_providers {
     feature-flag-eval = {
       version = "0.2"
-      source  = "launchdarkly/feature-flag-eval"
+      source  = "launchdarkly.com/launchdarkly/feature-flag-eval"
     }
   }
 }
 
 provider "feature-flag-eval" {
-  sdk_key = "bla"
+  sdk_key = "sdk-2aaff62c-d031-47d1-9594-add194fce944"
 }
 
-#data "launchdarkly_flag_evaluation_boolean" "mybool" {
-#  flagKey = "bla"
-#  default_value = false
-#  context = {
-#    key = "hosh@launchdarkly"
-#  }
-#}
-#
-#
-#locals {
-#  foo = file(yamlencode(data.launchdarkly_flag_evaluation_boolean.mybool))
-#}
+data "launchdarkly_flag_evaluation_boolean" "mybool" {
+  flagKey       = "boolean-flag"
+  default_value = false
+  context = {
+    key = "hosh@launchdarkly.com"
+  }
+}
+
+output "variation_value" {
+  value = data.launcharkly_flag_evaluation_boolean.mybool
+}
+
+
+locals {
+  foo = file(yamlencode(data.launchdarkly_flag_evaluation_boolean.mybool))
+}
