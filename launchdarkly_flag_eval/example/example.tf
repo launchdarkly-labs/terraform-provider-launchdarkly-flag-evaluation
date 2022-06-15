8a14d9918e79aefa763c9b543a8eb923f3aaf665
Mahek Chheda <mchheda@launchdarkly.com>
2022-06-15 16:23:23 -0400

    support tuples.

--- launchdarkly_flag_eval/example/example.tf
+++ launchdarkly_flag_eval/example/example.tf
@@ -20,8 +20,9 @@ data "feature-flag-eval_boolean" "mybool" {
       "test"  = "foo"
       "test2" = 123
       "test3" = false
-      "test4" = ["red", "orange", "yellow", true, 4] // tuple example
-      "test5" = tolist(["b", "w"]) // list example
+      // TODO: implement lists
+      "test4" = ["red", "orange", "yellow", true, 4]
+      "test5" = list(["black", "white"])
       // note: cannot pass JSON objects as custom properties
       // https://docs.launchdarkly.com/home/users/attributes
     }
